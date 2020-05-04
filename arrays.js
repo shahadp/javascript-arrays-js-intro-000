150bff2dafb6c92ace42c4e648e6f46ff21c7f14
Shahad Puthukudi <shahadp@gmail.com>
2020-05-04 21:39:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars=[
 ]
 function addElementToBeginningOfArray(array,element)
 {
-  let newArray=[element,...array]
+  var newArray=[element,...array]
   return newArray
 }
 function destructivelyAddElementToBeginningOfArray(array,element)
@@ -16,7 +16,7 @@ function destructivelyAddElementToBeginningOfArray(array,element)
 }
 function addElementToEndOfArray(array, element)
 {
-  let newArray=[...array,element]
+  var newArray=[...array,element]
   return newArray
 }
 destructivelyAddElementToEndOfArray(array, element)
